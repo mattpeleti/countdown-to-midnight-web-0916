6d601314f7b6d935ce580c343ff8dc509f9ad254
Matt Peleti <mattpeleti@gmail.com>
2016-09-25 02:24:15 +0000

    Countdowns in 2016?!

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,29 @@
 #write your code here
+def countdown(num)
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num-=1
+  end
+  if num==0
+    "HAPPY NEW YEAR!"
+  end
 
-def countdown
+    #if $counter == 0
+      #return "HAPPY NEW YEAR!"
+    #else
+      #puts "#{num} SECOND(S)!"
+    #end
+    #counter -= 1
+  #end
+end
+
+def countdown_with_sleep(num)
+  while num > 0
+    sleep(1)
+    puts "#{num} SECOND(S)!"
+    num-=1
+  end
+  if num==0
+    "HAPPY NEW YEAR!"
+  end
 end
